2e8e481c2262ef9d45823c12d1dc0f183c151576
GameWyrm <daronan97@hotmail.com>
2024-10-22 22:44:08 -0700

    Added WIP Doors

---
+++ EscapeRoomJam4/LockedDoor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace EscapeRoomJam4
+{
+    public class LockedDoor : MonoBehaviour
+    {
+        private void Start()
+        {
+            // for debugging only now, remove when we can actually want to be able to open them
+            SetOpenState(true);
+        }
+
+        public void SetOpenState(bool open)
+        {
+            Animator animator = GetComponent<Animator>();
+            if (open) animator.SetTrigger("Open");
+            else animator.SetTrigger("Closed");
+        }
+    }
+}
