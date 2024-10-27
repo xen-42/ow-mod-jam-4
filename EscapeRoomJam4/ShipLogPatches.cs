04656fb4693c30a453610033bc3a5936327c5cff
xen-42 <nicholasconnors@hotmail.ca>
2024-10-26 20:30:41 -0400

    Fix ship log ordering of fake planets, prevent moving upwards when not on the map mode index

--- EscapeRoomJam4/ShipLogPatches.cs
+++ EscapeRoomJam4/ShipLogPatches.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 
 namespace EscapeRoomJam4
 {
-
     [HarmonyPatch]
     public class ShipLogPatches
     {
         [HarmonyPrefix, HarmonyPatch(typeof(ShipLogController), nameof(ShipLogController.OnPressInteract))]
         public static void ShipLogController_OnPressInteract_Prefix(ShipLogController __instance)
         {
+            if (!EscapeRoomJam4.InEscapeSystem())
+            {
+                return;
+            }
+
             Transform mapModeRoot = __instance.transform.Find("ShipLogPivot/ShipLogCanvas/MapMode/ScaleRoot/PanRoot");
             foreach (Transform planet in mapModeRoot.transform)
             {
@@ -23,5 +27,40 @@ namespace EscapeRoomJam4
                 }
             }
         }
+
+        private static int _rowIndex, _objIndex;
+
+        [HarmonyPrefix, HarmonyPatch(typeof(ShipLogMapMode), nameof(ShipLogMapMode.UpdateMapNavigation))]
+        public static void ShipLogMapMode_UpdateMapNavigation(ShipLogMapMode __instance)
+        {
+            if (!EscapeRoomJam4.InEscapeSystem())
+            {
+                return;
+            }
+
+            // Track these because they get modified by the time ShipLogMapMode_GetClosestIndexInRow is called
+            _rowIndex = __instance._rowIndex;
+            _objIndex = __instance._objIndex;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(ShipLogMapMode), nameof(ShipLogMapMode.GetClosestIndexInRow))]
+        public static void ShipLogMapMode_GetClosestIndexInRow(ShipLogMapMode __instance, float xPos, int rowIndex, bool requireVisible, ref int __result)
+        {
+            if (!EscapeRoomJam4.InEscapeSystem())
+            {
+                return;
+            }
+
+            EscapeRoomJam4.Instance.ModHelper.Console.WriteLine($"Row {__instance._rowIndex}, col {__instance._objIndex}");
+            // Only allow the player to directly move up from row 1 at column 8
+            if (_rowIndex == 1 && _objIndex != 8)
+            {
+                __instance._rowIndex = 1;
+                __result = __instance._objIndex;
+            }
+        }
     }
 }
+
+
+
