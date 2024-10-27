f9f96a07d84602e96460ce192966539e9456bf3d
xen-42 <nicholasconnors@hotmail.ca>
2024-10-27 01:42:07 -0400

    Add warpvolume

---
+++ EscapeRoomJam4/WarpVolume.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace EscapeRoomJam4;
+
+/// <summary>
+/// Must be on a GameObject on the BasicEffectVolume layer, with a collider component with isTrigger = true
+/// </summary>
+public class WarpVolume : MonoBehaviour
+{
+    private PlayerCameraEffectController _effectController;
+
+    [SerializeField]
+    private Transform _respawnPoint;
+
+    public void Start()
+    {
+        _effectController = Locator.GetPlayerCamera().GetComponent<PlayerCameraEffectController>();
+    }
+
+    public void OnTriggerEnter(Collider hitCollider)
+    {
+        if (hitCollider.attachedRigidbody == Locator.GetPlayerBody()._rigidbody)
+        {
+            if (gameObject.activeInHierarchy)
+            {
+                WarpPlayer();
+            }
+        }
+    }
+
+    private IEnumerator WarpPlayer()
+    {
+        _effectController.CloseEyes(1f);
+        OWInput.ChangeInputMode(InputMode.None);
+        ReticleController.Hide();
+        Locator.GetPromptManager().SetPromptsVisible(false);
+
+        yield return new WaitForSeconds(1f);
+
+        OWInput.ChangeInputMode(InputMode.Character);
+
+        _effectController.OpenEyes(1f);
+        ReticleController.Show();
+        Locator.GetPromptManager().SetPromptsVisible(true);
+        Locator.GetPlayerBody().GetComponent<PlayerBody>().WarpToPositionRotation(_respawnPoint.position, _respawnPoint.rotation);
+    }
+}
