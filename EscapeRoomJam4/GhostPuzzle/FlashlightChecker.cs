10268c762465ad153ae912b1bc361e3d2b14c6e8
GameWyrm <daronan97@hotmail.com>
2024-10-31 21:56:18 -0700

    Improved various puzzles
    - Colored arrows in Layer 1 now have colorblind prompts
    - Made Ghost Puzzle a bit harder
    - Ghost Puzzle now has prompts to make what you're supposed to do a bit clearer

---
+++ EscapeRoomJam4/GhostPuzzle/FlashlightChecker.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace EscapeRoomJam4.GhostPuzzle
+{
+    public class FlashlightChecker : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject onFlashlightPrompt;
+        [SerializeField]
+        private GameObject offFlashlightParent;
+        [SerializeField]
+        private LookAtPrompt offFlashlightPrompt;
+
+        private DreamObjectProjector projector;
+
+        private void Start()
+        {
+            projector = GetComponentInParent<DreamObjectProjector>();
+            projector.OnProjectorExtinguished += ExtinguishPrompt;
+            projector.OnProjectorLit += LightPrompt;
+            onFlashlightPrompt.SetActive(true);
+            offFlashlightParent.SetActive(false);
+            offFlashlightPrompt.SetPromptActive(false);
+            GlobalMessenger.AddListener("TurnOnFlashlight", TurnOnFlashlight);
+            GlobalMessenger.AddListener("TurnOffFlashlight", TurnOffFlashlight);
+        }
+
+        private void Destroy()
+        {
+            GlobalMessenger.RemoveListener("TurnOnFlashlight", TurnOnFlashlight);
+            GlobalMessenger.RemoveListener("TurnOffFlashlight", TurnOffFlashlight);
+        }
+
+        public void ExtinguishPrompt()
+        {
+            onFlashlightPrompt.SetActive(true);
+            offFlashlightParent.SetActive(false);
+        }
+
+        public void LightPrompt()
+        {
+            onFlashlightPrompt.SetActive(false);
+            offFlashlightParent.SetActive(true);
+        }
+
+        public void TurnOffFlashlight()
+        {
+            offFlashlightPrompt.SetPromptActive(false);
+        }
+
+        public void TurnOnFlashlight()
+        {
+            offFlashlightPrompt.SetPromptActive(true);
+        }
+    }
+}
---
+++ EscapeRoomJam4/LookAtPrompt.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace EscapeRoomJam4
+{
+    public class LookAtPrompt : MonoBehaviour
+    {
+        [Tooltip("String ID that will be translated and show. Uses UI Dictionary.")]
+        public string promptText;
+
+        private SingleInteractionVolume interactionVolume;
+
+        private void Start()
+        {
+            string translatedPrompt = EscapeRoomJam4.Instance.NewHorizons.GetTranslationForUI(promptText);
+
+            interactionVolume = gameObject.GetAddComponent<SingleInteractionVolume>();
+            interactionVolume.SetKeyCommandVisible(false);
+            interactionVolume.ChangePrompt(translatedPrompt);
+            // prevent reaction to interact being pressed
+            interactionVolume.OnGainFocus += () => interactionVolume.ChangePrompt(translatedPrompt);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (GetComponent<InteractReceiver>() == null) return;
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(transform.position, GetComponent<InteractReceiver>()._interactRange);
+        }
+
+        public void SetPromptActive(bool active)
+        {
+            gameObject.SetActive(active);
+        }
+    }
+}
